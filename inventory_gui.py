33691bc074c5a0de80257a1d351464eb0d5aaad8
lbrooks2390 <lbrooks2390@gmail.com>
2025-12-15 09:18:17 -0600

    Update inventory_gui.py

--- inventory_gui.py
+++ inventory_gui.py
@@ -4,14 +4,14 @@
 ## Form generated from reading UI file 'gui.ui'
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+from PyQt6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+from PyQt6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QPushButton,
+from PyQt6.QtWidgets import (QApplication, QLabel, QLineEdit, QPushButton,
     QSizePolicy, QTextBrowser, QWidget)
 
 class Ui_Inventory(object):
